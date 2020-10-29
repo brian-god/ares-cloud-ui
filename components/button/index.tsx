257f48cdb238af0239fcbf587c2ea8af7f7ccb53
txk <tangxkwork@163.com>
2020-10-29 15:34:49 +0800

    加入按钮

---
+++ components/button/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Button} from 'antd'
+
+const AresButton: React.FC = ({ children, ...rest }) => (
+  <Button>插件测试按钮</Button>
+);
+
+export default AresButton;
---
+++ components/index.tsx
@@ -0,0 +1,5 @@
+import Alert from './alert';
+import AresButton from './button'
+export {
+    Alert,AresButton
+}
